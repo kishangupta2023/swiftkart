14ab37c43294001478d140075896df45fc7f5238
kishan <kishangupta3003@gmail.com>
2026-01-09 19:08:10 +0530

    feat: implement admin and user dashboards, add grocery management features, and integrate Cloudinary for image uploads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,39 @@
 import { auth } from "@/auth";
+import AdminDashboard from "@/components/AdminDashboard";
+import DeliveryBoy from "@/components/DeliveryBoy";
 import EditRoleMobile from "@/components/EditRoleMobile";
 import Nav from "@/components/Nav";
+import UserDashboard from "@/components/UserDashboard";
 import connectDb from "@/lib/db";
 import User from "@/models/user.model";
 import { redirect } from "next/navigation";
 
 async function Home() {
-  await connectDb()
-  const session = await auth()
-  const user = await User.findById(session?.user?.id)
-  if(!user){
-    redirect("/login")
+  await connectDb();
+  const session = await auth();
+  const user = await User.findById(session?.user?.id);
+  if (!user) {
+    redirect("/login");
   }
 
-  const inComplete = !user.mobile || !user.role || (!user.mobile && user.role=="user")
-  if(inComplete){
-    return <EditRoleMobile/>
+  const inComplete =
+    !user.mobile || !user.role || (!user.mobile && user.role == "user");
+  if (inComplete) {
+    return <EditRoleMobile />;
   }
+  const plainUser = JSON.parse(JSON.stringify(user));
+
   return (
     <>
-    <Nav user={user}/>
+      <Nav user={plainUser} />
+      {user.role === "user" ? (
+        <UserDashboard />
+      ) : user.role == "admin" ? (
+        <AdminDashboard />
+      ) : (
+        <DeliveryBoy />
+      )}
     </>
   );
 }
-export default Home
+export default Home;
