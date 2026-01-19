03758410cf7e7d36cd1e863d40d7877a1e455df0
kishan <kishangupta3003@gmail.com>
2026-01-19 20:06:29 +0530

    feat: integrate Redux for state management and add user authentication
    - Added Redux store with user and cart slices for managing application state.
    - Implemented user authentication API endpoint to fetch user data.
    - Created InitUser component to initialize user state on app load.
    - Developed cart functionality with add, remove, and quantity adjustment features.
    - Introduced GroceryItemCard and CategorySlider components for displaying grocery items.
    - Enhanced layout with StoreProvider to wrap the application in Redux context.
    - Updated global styles to include scrollbar hiding.
    - Added Checkout and Cart pages for managing user shopping experience.
    - Implemented hooks for fetching user data and managing cart state.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Provider from "@/provider";
+import StoreProvider from "@/redux/StoreProvider";
+import InitUser from "@/InitUser";
 
 export const metadata: Metadata = {
   title: "SwiftKart | 10 minutes grocery Delivery App",
@@ -14,9 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="w-full min-h-screen bg-linear-to-b from-green-50 to-white">
+      <body className="w-full min-h-[200vh] bg-linear-to-b from-green-50 to-white">
         <Provider>
-        {children}
+        <StoreProvider>
+          <InitUser/>
+           {children}
+        </StoreProvider>
         </Provider>
       </body>
     </html>
